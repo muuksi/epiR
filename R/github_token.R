f9b1482b48cd1edd70d7734ba0d5dc34a547cf05
Maximilian Hinse <maximilianhinse@epi03187568.charite.de>
2024-04-11 10:45:31 +0200

    new proxy function für charité netz

--- R/github_token.R
+++ R/github_token.R
@@ -19,3 +19,20 @@ set_token <- function(){
   gitcreds_get()
 }
 
+
+proxy <- function(url = "http://proxy.charite.de:8080"){
+  proxy_site <- url
+  check_proxy <- suppressWarnings(try(open.connection(url(proxy_site),open="rt",timeout=1),silent=T)[1])
+  suppressWarnings(try(close.connection(proxy_site),silent=T))
+  #print(check_proxy)
+  if (is.null(check_proxy)) {
+    print ("Achtung ! Proxy gefunden. Ich setze die Env-Variable auf den Proxy ...")
+    Sys.setenv(https_proxy=proxy_site)   # hier wird die https-Variable gesetzt !
+  } else {
+    print ("kein Proxy zu sehen ... Env-Variable wird verworfen")
+    Sys.setenv(https_proxy="")
+  }
+}
+
+
+#Sys.getenv("https_proxy")
