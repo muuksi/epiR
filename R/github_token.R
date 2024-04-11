fb38d35c2a1d5c8ea8daff29d2abbc6e0570acb1
Maximilian Hinse <maximilianhinse@epi03187568.charite.de>
2024-04-11 10:46:06 +0200

    anpassen der proxy function

--- R/github_token.R
+++ R/github_token.R
@@ -5,7 +5,7 @@
 
 require(usethis)
 require(gitcreds)
-
+require(httr)
 
 get_newgithubtoken <- function(){
   usethis::create_github_token()
@@ -20,19 +20,20 @@ set_token <- function(){
 }
 
 
-proxy <- function(url = "http://proxy.charite.de:8080"){
+proxy <- function(url = "http://proxy.charite.de"){
   proxy_site <- url
   check_proxy <- suppressWarnings(try(open.connection(url(proxy_site),open="rt",timeout=1),silent=T)[1])
   suppressWarnings(try(close.connection(proxy_site),silent=T))
   #print(check_proxy)
   if (is.null(check_proxy)) {
     print ("Achtung ! Proxy gefunden. Ich setze die Env-Variable auf den Proxy ...")
-    Sys.setenv(https_proxy=proxy_site)   # hier wird die https-Variable gesetzt !
+    Sys.setenv(https_proxy="http://proxy.charite.de:8080")   # hier wird die https-Variable gesetzt !
+    set_config(use_proxy(url="proxy.charite.de", port = 8080)   )        # httr config eintragen
   } else {
     print ("kein Proxy zu sehen ... Env-Variable wird verworfen")
     Sys.setenv(https_proxy="")
   }
 }
 
-
+#proxy()
 #Sys.getenv("https_proxy")
